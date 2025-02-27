a8b6d6ccdf5b7ace8469cf16b1d0926d4cd20cf8
MrMataPacka <theangelpluschannel@gmail.com>
2025-02-26 20:29:54 -0600

    Cambios2

---
+++ Assets/Course/11_ControlDeVersiones/example.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Course.ControlDeVersiones
+{
+    public class example : MonoBehaviour
+    {
+        int id;
+
+        private void Start ()
+        {
+            id = 1;
+        }
+    }
+}
